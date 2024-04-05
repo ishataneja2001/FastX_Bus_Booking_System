181b89f77fe28e682d0c3838feafc7f3e9432b59
sainischay <sainischay20@gmail.com>
2024-04-05 10:57:19 +0530

    Added Testing Service for All the Repos

---
+++ FastXBookingTest/BusRepoTest.cs
@@ -0,0 +1,89 @@
+﻿using FastXBookingSample.Models;
+using FastXBookingSample.Repository;
+using Microsoft.AspNetCore.JsonPatch;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastXBookingTest
+{
+    [TestFixture]
+    public class BusRepoTest
+    {
+        private Mock<IBusRepository> _busRepository;
+
+        [SetUp]
+        public void Setup()
+        {
+            _busRepository = new Mock<IBusRepository>();
+        }
+
+
+        [Test]
+        public async Task TestForDeleteBus()
+        {
+            _busRepository.Setup(x => x.DeleteBus(It.IsAny<int>())).Returns("Deleted Successfully");
+            string output = await Task.Run(() => _busRepository.Object.DeleteBus(2));
+            Assert.AreEqual(output, "Deleted Successfully");
+        }
+
+
+        [Test]
+        public async Task TestForGetAll()
+        {
+            _busRepository.Setup(x => x.GetAll()).Returns(new List<Bus>()
+            {
+                new Bus()
+                {
+                   BusName = "Volvo",
+                   BusNumber = "TS04TG5478"
+                },
+               new Bus()
+                {
+                   BusName = "Super Luxury",
+                   BusNumber = "TS08HS2578"
+                }
+            }
+               );
+            List<Bus> output = await Task.Run(() => _busRepository.Object.GetAll());
+            Assert.AreEqual(output[0].BusName, "Volvo");
+            Assert.AreEqual(output[0].BusNumber, "TS04TG5478");
+            Assert.AreEqual(output[1].BusName, "Super Luxury");
+            Assert.AreEqual(output[1].BusNumber, "TS08HS2578");
+        }
+
+
+
+        [Test]
+        public async Task TestForPatchBus()
+        {
+            JsonPatchDocument<Bus> jsonpatch = new JsonPatchDocument<Bus>();
+            _busRepository.Setup(x => x.PatchBus(It.IsAny<int>(), jsonpatch)).Returns("Updated Successfully");
+            string output = await Task.Run(() => _busRepository.Object.PatchBus(It.IsAny<int>(), jsonpatch));
+            Assert.AreEqual(output, "Updated Successfully");
+        }
+
+
+        [Test]
+        public async Task TestForPostBus()
+        {
+            Bus bus = new Bus();
+            _busRepository.Setup(x => x.CreateBus(bus)).Returns("Added Successfully");
+            string output = await Task.Run(() => _busRepository.Object.CreateBus(bus));
+            Assert.AreEqual(output, "Added Successfully");
+        }
+
+
+        [Test]
+        public async Task TestForUpdateBoardingPoint()
+        {
+            Bus bus = new Bus();
+            _busRepository.Setup(x => x.UpdateBus(It.IsAny<int>(), bus)).Returns("Updated Successfully");
+            string output = await Task.Run(() => _busRepository.Object.UpdateBus(It.IsAny<int>(), bus));
+            Assert.AreEqual(output, "Updated Successfully");
+        }
+    }
+}
