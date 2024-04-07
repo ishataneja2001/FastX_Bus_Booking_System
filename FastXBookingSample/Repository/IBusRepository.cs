e62277988709bf21ef9b1b7c7f0885a5b177827a
sainischay <sainischay20@gmail.com>
2024-04-07 08:02:45 +0530

    Resolved issues for GetBusById

--- FastXBookingSample/Repository/IBusRepository.cs
+++ FastXBookingSample/Repository/IBusRepository.cs
@@ -9,7 +9,7 @@ namespace FastXBookingSample.Repository
         string DeleteBus(int id);
         string UpdateBus(int id, Bus bus);
         List<Bus> GetAll();
-        Task<Bus> GetBusById(int id);
+        Bus GetBusById(int id);
         List<Bus> GetBusByDetails(string origin, string destination, DateOnly date);
         bool BusExists(int id);
         bool RoleExists(int id);
